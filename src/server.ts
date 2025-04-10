a5bd20fca9b8b5fa63ab5b751947fb720bc4ea08
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-04-09 23:09:11 -0300

    configurações express

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
-const http = require('http');
+import express from "express"
 
-const server = http.createServer((req, res) => {
-   res.setHeader('Content-Type', 'text/plain');
- 
-   if (req.url === '/') {
-       res.statusCode = 200;
-       res.end('Home page');
-   } else if (req.url === '/sobre') {
-       res.statusCode = 200;
-       res.end('About page');
-   }
+const port = 3000;
+const app = express();
+
+app.get("/movies", (req, res) => {
+    res.send("Listagem de filmes")
 });
 
-server.listen(3000, () => {
- console.log(`Servidor em execução em http://localhost:3000/`);
+app.listen(port, () => {
+    console.log(`Servidor em execução na porta ${port}`);
 });
