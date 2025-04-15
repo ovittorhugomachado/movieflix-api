77620fb69e23f90bb9cd35eaee6bcf7761a9848f
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-04-15 10:32:36 -0300

    adicionada remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -71,6 +71,25 @@ app.put("/movies/:id", async (req, res) => {
 
 });
 
+app.delete("/movies/:id", async (req, res) => {
+
+    const id = Number(req.params.id)
+
+    try {
+        const movie = await prisma.movie.findUnique({ where: { id } });
+
+        if (!movie) {
+            return res.status(404).send({ message: "Filme não encontrado" })
+        }
+
+        await prisma.movie.delete({ where: { id } });
+    }catch(error) {
+        return res.status(500).send({ message: "Não foi possível deletar o filme"})
+    }
+
+    res.status(200).send()
+})
+
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
 });
