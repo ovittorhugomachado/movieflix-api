7a1230c0fca6413976d05b3d55bf51242cd8d239
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-04-15 10:12:39 -0300

    adicionada atualização de filmes

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,7 @@ app.post("/movies", async (req, res) => {
 
     try {
         const moviesWithSameName = await prisma.movie.findFirst({
-            where: { movie_name: {equals: movie_name, mode: "insensitive"} }
+            where: { movie_name: { equals: movie_name, mode: "insensitive" } }
         });
 
         if (moviesWithSameName) {
@@ -47,6 +47,29 @@ app.post("/movies", async (req, res) => {
 
     res.status(201).send()
 })
+
+app.put("/movies/:id", async (req, res) => {
+
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: { id },
+        });
+
+        if (!movie) {
+            return res.status(404).send({ message: "Filme não encontrado" });
+        }
+
+        const data = { ...req.body };
+        data.release_date = data.release_date
+            ? new Date(data.release_date)
+            : undefined;
+
+        await prisma.movie.update({ where: { id }, data });
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao atualizar o registro" });
+    }
+
+});
 
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
