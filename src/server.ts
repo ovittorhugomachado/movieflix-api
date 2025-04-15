b531a834961c483714e5ecddcafa8aa6fb78e607
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-04-15 08:57:29 -0300

    adicionada correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express"
-import { PrismaClient } from './generated/prisma'
+import { PrismaClient } from './generated/prisma/client'
 
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(express.json())
+
 app.get("/movies", async (_, res) => {
+
     const movies = await prisma.movie.findMany({
         orderBy: {
             movie_name: "asc"
@@ -15,8 +18,35 @@ app.get("/movies", async (_, res) => {
             languages: true
         }
     });
+
     res.json(movies);
 });
+
+app.post("/movies", async (req, res) => {
+    const { movie_name, movie_language, genre_movie } = req.body
+
+    try {
+        const moviesWithSameName = await prisma.movie.findFirst({
+            where: { movie_name: {equals: movie_name, mode: "insensitive"} }
+        });
+
+        if (moviesWithSameName) {
+            return res.status(409).send({ message: "Jáexiste um filme cadastrado com esse título" });
+        };
+
+        await prisma.movie.create({
+            data: {
+                movie_name,
+                movie_language,
+                genre_movie
+            }
+        });
+    } catch (error) {
+        return res.status(500).send({ message: "ocorreu um erro ao cadastrar filme" });
+    }
+
+    res.status(201).send()
+})
 
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
